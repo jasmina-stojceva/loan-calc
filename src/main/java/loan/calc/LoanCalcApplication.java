afb73f901be2303227c97f62601743f9c6cd7d14
Jasmina Stojcheva <jasmina.stojceva@gmail.com>
2022-04-12 22:54:31 +0200

    first commit

---
+++ src/main/java/loan/calc/LoanCalcApplication.java
@@ -0,0 +1,13 @@
+package loan.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCalcApplication.class, args);
+	}
+
+}
